00a27f83d2cf75cc411a6fe386faf0d2e8fe2d00
Joel <joeltwh1999@gmail.com>
2021-12-24 21:49:17 +0800

    section example

---
+++ src/sections/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function AboutSection() {
+    return (
+        <div className="about">
+            <h1>This is the about section</h1>
+            <p>Hello there</p>
+        </div>
+    )
+}
+
+export default AboutSection;
